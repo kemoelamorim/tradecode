54c510d413076d7194f12dda2b0f7b3dfae30064
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2022-12-15 23:53:14 -0300

    arquivos git

---
+++ src/main/java/br/com/tradecode/TradecodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tradecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tradecode/TradecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tradecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
